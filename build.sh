bba452756b1089a14dd217a968a9d441485cd71d
ocoot <cot48s@gmail.com>
2019-02-09 19:00:00 +0700

    octopus_aosp_mido: build.sh: updating script

--- build.sh
+++ build.sh
@@ -5,23 +5,23 @@ export KBUILD_BUILD_USER="octo21" # Build Host
 export KBUILD_BUILD_HOST="lineageOS" # Build Name
 REPACK_DIR=/home/octo21/android/octopus_aosp_mido/AnyKernel2
 OUT=$KERNEL_DIR/out
-ZIP_NAME="$VERSION"-v3.0-"$DATE"
+ZIP_NAME="$VERSION"-"$DATE"
 VERSION="mido"
 DATE=`date +"%Y%m%d"`
 export ARCH=arm64 && export SUBARCH=arm64
 export CROSS_COMPILE="/home/octo21/android/aarch64-linux-android/bin/aarch64-linux-android-"
 export CROSS_COMPILE_ARM32="/home/octo21/android/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
-export STRIP_KO="/home/octo21/android/aarch64-linux-android/aarch64-common-linux-android/bin/strip"
+export STRIP_KO="/home/octo21/android/aarch64-linux-android/aarch64-unknown-linux-android/bin/strip"
 
 make_zip()
 {
 		cd $REPACK_DIR
 		cp $KERNEL_DIR/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-nontreble.dtb $REPACK_DIR/treble-unsupported/
 		cp $KERNEL_DIR/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-treble.dtb $REPACK_DIR/treble-supported/
-		cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz $REPACK_DIR/
-		FINAL_ZIP="${VERSION}-octopus-${DATE}.zip"
+		cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz $REPACK_DIR/kernel/
+		FINAL_ZIP="${VERSION}-octopus-v3.0-${DATE}.zip"
 		zip -r9 "${FINAL_ZIP}" *
-		cp *.zip $OUT
+		cp *.zip $KERNEL_DIR/build/
 		rm *.zip
 		cd $KERNEL_DIR
 }
